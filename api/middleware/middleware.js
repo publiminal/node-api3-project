b079c028757b0c88a1f975abaaed16f2e09dd2b7
publiminal <publiminal@gmail.com>
2022-05-12 01:29:45 -0400

    added logger

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,20 @@
+
+/* 
+logger()
+logger logs to the console the following information about each request: 
+request method, request url, and a timestamp
+this middleware runs on every request made to the API
+
+*/
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  // console.log(`this is logger in action`)
+  // request method : req.method
+  // request url : req.url
+  // const timestamp = Date.now()
+  const log = `${req.method} => ${req.url} :: ${Math.floor(Date.now()/1000)}`
+  console.log(log)
 }
 
 function validateUserId(req, res, next) {
@@ -15,3 +30,5 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+
+module.exports = {logger, validateUserId, validateUser, validatePost}
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it
+const server = require('./api/server')
+
+const port = 5000
+server.listen( port, () => { 
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`) 
+})
